a8516b3709febc67d26fdde5d65aeb13f4e40f86
Israel Hespanhol <israel.hespanhol@example.com>
2026-09-08 15:41:08 -0300

    Fix Prisma Client instantiation at build time when env variables are not present

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,9 +2,11 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
+const dbUrl = process.env.DIRECT_URL ?? process.env.DATABASE_URL;
+
 export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-	log: ["error"],
-	datasources: { db: { url: process.env.DIRECT_URL ?? process.env.DATABASE_URL } },
+  log: ['error'],
+  ...(dbUrl ? { datasources: { db: { url: dbUrl } } } : {})
 });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
